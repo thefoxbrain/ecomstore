a4a8a4190573faa2b881513469ba506cdeb4fb8e
Rob Fox <thefoxbrain@gmail.com>
2013-07-23 18:02:15 +0100

    format cart, validation

--- cart/views.py
+++ cart/views.py
@@ -3,7 +3,13 @@ from django.template import RequestContext
 import cart 
 
 def show_cart(request, template_name="cart/cart.html"): 
-    # cart_item_count = cart.cart_item_count(request) 
-    cart_item_count = cart.cart_distinct_item_count(request)
+    if request.method == 'POST': 
+        postdata = request.POST.copy() 
+        if postdata['submit'] == 'Remove': 
+            cart.remove_from_cart(request) 
+        if postdata['submit'] == 'Update': 
+            cart.update_cart(request) 
+    cart_items = cart.get_cart_items(request) 
     page_title = 'Shopping Cart' 
+    cart_subtotal = cart.cart_subtotal(request) 
     return render_to_response(template_name, locals(),context_instance=RequestContext(request))
